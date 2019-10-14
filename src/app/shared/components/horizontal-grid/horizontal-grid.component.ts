14b075c83812264b2b94910faecd550eb58732b2
guojianglin <1448373124@qq.com>
2019-10-14 22:58:37 +0800

    组件投影的练习

--- src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
+++ src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter, ElementRef, HostBinding, Renderer2, AfterViewInit, AfterContentInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, HostBinding,
+  Renderer2, AfterViewInit, AfterContentInit } from '@angular/core';
 
 export interface Channel {
   id: number;
@@ -15,56 +16,7 @@ export interface Channel {
 export class HorizontalGridComponent implements OnInit, AfterViewInit, AfterContentInit {
   // @HostBinding('style.display') display = 'block';
 
-  channels: Channel[] = [
-    {
-      id: 1,
-      icon: 'icon-zhiwu',
-      title: '植物',
-      link: ''
-    },
-    {
-      id: 2,
-      icon: 'icon-naozhong',
-      title: '闹钟',
-      link: ''
-    },
-    {
-      id: 3,
-      icon: 'icon-qiche',
-      title: '汽车',
-      link: ''
-    },
-    {
-      id: 4,
-      icon: 'icon-lipin',
-      title: '礼品',
-      link: ''
-    },
-    {
-      id: 5,
-      icon: 'icon-jishiben',
-      title: '记事本',
-      link: ''
-    },
-    {
-      id: 6,
-      icon: 'icon-diqiuyi',
-      title: '地球仪',
-      link: ''
-    },
-    {
-      id: 7,
-      icon: 'icon-dianshiji',
-      title: '电视机',
-      link: ''
-    },
-    {
-      id: 8,
-      icon: 'icon-biaoqing',
-      title: '表情',
-      link: ''
-    },
-  ];
+
   constructor(
     private elr: ElementRef,
     private rd2: Renderer2
