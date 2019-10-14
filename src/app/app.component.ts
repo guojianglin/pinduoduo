f70c957015393ed317368d137dd4bc13e15a6417
guojianglin <1448373124@qq.com>
2019-10-14 22:58:59 +0800

    组件投影的练习

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ImageSlider, TopMenu } from './shared/components';
+import { ImageSlider, TopMenu, Channel } from './shared/components';
 
 
 @Component({
@@ -77,6 +77,56 @@ export class AppComponent {
     {
       title: '美妆',
       link: '1'
+    },
+  ];
+  channels: Channel[] = [
+    {
+      id: 1,
+      icon: 'icon-zhiwu',
+      title: '植物',
+      link: ''
+    },
+    {
+      id: 2,
+      icon: 'icon-naozhong',
+      title: '闹钟',
+      link: ''
+    },
+    {
+      id: 3,
+      icon: 'icon-qiche',
+      title: '汽车',
+      link: ''
+    },
+    {
+      id: 4,
+      icon: 'icon-lipin',
+      title: '礼品',
+      link: ''
+    },
+    {
+      id: 5,
+      icon: 'icon-jishiben',
+      title: '记事本',
+      link: ''
+    },
+    {
+      id: 6,
+      icon: 'icon-diqiuyi',
+      title: '地球仪',
+      link: ''
+    },
+    {
+      id: 7,
+      icon: 'icon-dianshiji',
+      title: '电视机',
+      link: ''
+    },
+    {
+      id: 8,
+      icon: 'icon-biaoqing',
+      title: '表情',
+      link: ''
     },
   ];
   imageSlider: ImageSlider[] = [
