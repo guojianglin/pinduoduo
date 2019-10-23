13e89c8dd1dbf1c4070c62fa52b0689cedb99ae5
guojianglin <1448373124@qq.com>
2019-10-24 07:55:08 +0800

    投影组件和grid布局的配合使用

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -128,6 +128,54 @@ export class AppComponent {
       title: '表情',
       link: ''
     },
+    {
+      id: 1,
+      icon: 'icon-zhiwu',
+      title: '植物',
+      link: ''
+    },
+    {
+      id: 2,
+      icon: 'icon-naozhong',
+      title: '闹钟',
+      link: ''
+    },
+    {
+      id: 3,
+      icon: 'icon-qiche',
+      title: '汽车',
+      link: ''
+    },
+    {
+      id: 4,
+      icon: 'icon-lipin',
+      title: '礼品',
+      link: ''
+    },
+    {
+      id: 5,
+      icon: 'icon-jishiben',
+      title: '记事本',
+      link: ''
+    },
+    {
+      id: 6,
+      icon: 'icon-diqiuyi',
+      title: '地球仪',
+      link: ''
+    },
+    {
+      id: 7,
+      icon: 'icon-dianshiji',
+      title: '电视机',
+      link: ''
+    },
+    {
+      id: 8,
+      icon: 'icon-biaoqing',
+      title: '表情',
+      link: ''
+    },
   ];
   imageSlider: ImageSlider[] = [
     {
--- src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
+++ src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
@@ -14,7 +14,21 @@ export interface Channel {
   styleUrls: ['./horizontal-grid.component.scss']
 })
 export class HorizontalGridComponent implements OnInit, AfterViewInit, AfterContentInit {
-  // @HostBinding('style.display') display = 'block';
+  @Input() cols = 8;
+  @Input() displayCols = 5;
+  sliderMargin = '0';
+
+  public get scrollable(): boolean {
+    return this.cols > this.displayCols;
+  }
+
+  public get templateRows(): string {
+    return 'minmax(auto, max-content)';
+  }
+
+  public get templateCols(): string {
+    return `repeat(${this.cols}, calc((100vw - ${this.displayCols} * 0.4rem) / ${this.displayCols}))`;
+  }
 
 
   constructor(
@@ -32,5 +46,10 @@ export class HorizontalGridComponent implements OnInit, AfterViewInit, AfterCont
   }
   ngAfterViewInit() {
     // this.rd2.setStyle(this.elr.nativeElement, 'display', 'flex');
+  }
+
+  handleScroll(evt) {
+    this.sliderMargin = `0 ${100 * evt.target.scrollLeft / evt.target.scrollWidth}%`;
+    console.log('sliderMargin', this.sliderMargin)
   }
 }
